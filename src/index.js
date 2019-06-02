163541848b0fbc8f14aac63167bee645a88411d3
Victoria Rautenbach <victoria.rautenbach@gmail.com>
2019-06-02 09:01:36 +0200

    Add HashRouter

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import logo from './animated_logo0_small.gif';
